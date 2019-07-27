c2d241a10d06133b4c10200ee9dd379574616710
ej550 <edward.oughton@gmail.com>
2019-07-27 13:10:49 +0100

    Add updates to adapt for ruby

--- src/itmlogic/aknfe.py
+++ src/itmlogic/aknfe.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def aknfe(v2):
     """
-    Returns knife edge diffraction loss 
+    Returns knife edge diffraction loss
 
     Parameters
     ----------
@@ -17,11 +17,12 @@ def aknfe(v2):
 
     """
     if v2 < 5.76:
-        
+        if v2 <= 0: ### addition to avoid logging v2 <= 0
+            v2 = 0.00001 ### addition to avoid logging v2 <= 0
         aknfe1 = 6.02 + 9.11 * math.sqrt(v2) - 1.27 * v2
-    
+
     else:
-    
+
         aknfe1 = 12.953 + 4.343 * np.log(v2)
-    
+
     return aknfe1
