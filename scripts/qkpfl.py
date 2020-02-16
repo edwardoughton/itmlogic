373a363723fa882304e7046c56dabde9befcb86d
ej550 <edward.oughton@gmail.com>
2020-02-16 14:45:58 +0000

    Remove unused paths

--- scripts/qkpfl.py
+++ scripts/qkpfl.py
@@ -29,8 +29,6 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(os.path.dirname(__file__), 'script_config.ini'))
 BASE_PATH = CONFIG['file_locations']['base_path']
 
-DATA_RAW = os.path.join(BASE_PATH, 'raw')
-DATA_INTERMEDIATE = os.path.join(BASE_PATH, 'intermediate')
 DATA_PROCESSED = os.path.join(BASE_PATH, 'processed')
 
 
