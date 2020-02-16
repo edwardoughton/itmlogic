c4b3a843d6591ae0b252b7c4d69ce43ce27b42b4
ej550 <edward.oughton@gmail.com>
2020-02-16 23:19:09 +0000

    Add qlrpfl test

--- tests/test_qlrpfl.py
+++ tests/test_qlrpfl.py
@@ -3,7 +3,9 @@ from itmlogic.qlrpfl import qlrpfl
 
 def test_qlrpfl(
     setup_prop_to_test_qlrpfl,
-    setup_final_prop_to_test_qlrpfl
+    setup_final_prop_to_test_qlrpfl,
+    setup_prop_to_test_qlrpfl_pimter,
+    setup_final_prop_to_test_qlrpfl_pimter,
     ):
 
     actual_answer = qlrpfl(setup_prop_to_test_qlrpfl)
@@ -18,3 +20,15 @@ def test_qlrpfl(
     assert actual_answer['klim'] == expected_answer['klim']
     assert actual_answer['he'] == expected_answer['he']
     assert actual_answer['gme'] == expected_answer['gme']
+
+    actual_answer = qlrpfl(setup_prop_to_test_qlrpfl_pimter)
+
+    expected_answer = setup_final_prop_to_test_qlrpfl_pimter
+
+    assert actual_answer['dist'] == expected_answer['dist']
+    assert actual_answer['the'] == expected_answer['the']
+    assert actual_answer['dl'] == expected_answer['dl']
+    assert actual_answer['mdp'] == expected_answer['mdp']
+    assert actual_answer['lvar'] == expected_answer['lvar']
+    assert actual_answer['he'] == expected_answer['he']
+    assert actual_answer['aref'] == expected_answer['aref']
