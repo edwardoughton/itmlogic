0cb832d0714b526619b93310196952f89cdca376
ej550 <edward.oughton@gmail.com>
2020-02-16 16:16:20 +0000

    Add additional test

--- tests/test_aknfe.py
+++ tests/test_aknfe.py
@@ -3,6 +3,7 @@ from itmlogic.aknfe import aknfe
 
 def test_aknfe():
 
+    assert round(aknfe(-1), 2) == 6.05
     assert round(aknfe(2), 2) == 16.36
     assert round(aknfe(3), 2) == 17.99
     assert round(aknfe(5), 2) == 20.04
