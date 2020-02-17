f9d7f747e2e03dafd2b1cbd9730ebaaa48f4a034
ej550 <edward.oughton@gmail.com>
2020-02-17 11:27:40 +0000

    Add test for lrprop

--- tests/test_lrprop.py
+++ tests/test_lrprop.py
@@ -4,6 +4,7 @@ from itmlogic.lrprop import lrprop
 def test_lrprop(
     setup_prop_to_test_lrprop,
     setup_expected_prop_to_test_lrprop,
+    setup_prop_to_test_lrprop_2,
     setup_prop_to_test_lrprop_uarea,
     setup_expected_prop_to_test_lrprop_uarea
     ):
@@ -16,6 +17,10 @@ def test_lrprop(
     assert actual_prop['kwx'] == pytest.approx(expected_prop['kwx'])
     assert actual_prop['ael'] == pytest.approx(expected_prop['ael'])
     assert actual_prop['aref'] == pytest.approx(expected_prop['aref'])
+
+    actual_prop = lrprop(0, setup_prop_to_test_lrprop_2)
+
+    assert round(actual_prop['aref'], 2) == 35.42
 
     actual_prop = lrprop(10000, setup_prop_to_test_lrprop_uarea)
 
