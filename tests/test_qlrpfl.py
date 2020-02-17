3c9c2bafbd8319f2f3e1077de11a383474a47a9a
ej550 <edward.oughton@gmail.com>
2020-02-17 10:01:40 +0000

    Add in additional test

--- tests/test_qlrpfl.py
+++ tests/test_qlrpfl.py
@@ -6,6 +6,8 @@ def test_qlrpfl(
     setup_final_prop_to_test_qlrpfl,
     setup_prop_to_test_qlrpfl_pimter,
     setup_final_prop_to_test_qlrpfl_pimter,
+    setup_prop_to_test_qlrpfl_pimter_5,
+    setup_final_prop_to_test_qlrpfl_pimter_5
     ):
 
     actual_answer = qlrpfl(setup_prop_to_test_qlrpfl)
@@ -24,6 +26,18 @@ def test_qlrpfl(
     actual_answer = qlrpfl(setup_prop_to_test_qlrpfl_pimter)
 
     expected_answer = setup_final_prop_to_test_qlrpfl_pimter
+
+    assert actual_answer['dist'] == expected_answer['dist']
+    assert actual_answer['the'] == expected_answer['the']
+    assert actual_answer['dl'] == expected_answer['dl']
+    assert actual_answer['mdp'] == expected_answer['mdp']
+    assert actual_answer['lvar'] == expected_answer['lvar']
+    assert actual_answer['he'] == expected_answer['he']
+    assert actual_answer['aref'] == expected_answer['aref']
+
+    actual_answer = qlrpfl(setup_prop_to_test_qlrpfl_pimter_5)
+
+    expected_answer = setup_final_prop_to_test_qlrpfl_pimter_5
 
     assert actual_answer['dist'] == expected_answer['dist']
     assert actual_answer['the'] == expected_answer['the']
